999ac1f003739131b8bab0447355ddbe907c88fe
Yukun Xie <yxie@lcs.on.ca>
2024-01-12 08:45:22 -0500

    Linear gradient

--- Home/ContentView.swift
+++ Home/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            //Background
+            LinearGradient(
+                colors: [
+                    Color.red,
+                    Color.blue
+                ],
+                startPoint: .leading,
+                endPoint: .trailing
+            )
+            
+            //Foreground
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
